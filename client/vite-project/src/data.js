b18c9b7621829c2cdd46b6889c3a9558c86b22bc
Nishita Pande <pande.nishita@gmail.com>
2023-11-25 22:18:39 +0530

    made popular genre for movies section

--- client/vite-project/src/data.js
+++ client/vite-project/src/data.js
@@ -1,4 +1,4 @@
-export const moviesGenre = [
+export const popularMoviesGenre = [
     {
         id: 1,
         name: "Action",
@@ -29,59 +29,4 @@ export const moviesGenre = [
         name: "Sci-fi",
         img: "https://i.pinimg.com/originals/47/7c/31/477c318cd8918ed8ce8d49656e4f2a2f.jpg"
     },
-    {
-        id: 7,
-        name: "Western",
-        img: "https://i.pinimg.com/originals/64/23/f6/6423f6f83a7494c295b1f6059603ca0b.jpg"
-    },
-    {
-        id: 8,
-        name: "Animation",
-        img: "https://i.pinimg.com/originals/30/97/04/3097044f5906d9374f134fad44c377c6.jpg"
-    },
-    {
-        id: 9,
-        name: "Drama",
-        img: "https://i.pinimg.com/originals/34/8c/b1/348cb1c8539e4dac02119c98dfed16e6.jpg"
-    },
-    {
-        id: 10,
-        name: "Documentary",
-        img: "https://i.pinimg.com/originals/3a/db/bb/3adbbbf013f3b31cb23ad63e35ddab0d.jpg"
-    },
-    {
-        id: 11,
-        name: "Fantasy",
-        img: "https://i.pinimg.com/originals/64/f0/e3/64f0e34796e368c102b78ef7824f79a4.jpg"
-    },
-    {
-        id: 12,
-        name: "Adventure",
-        img: "https://i.pinimg.com/originals/dc/81/04/dc8104169da044b6526422c1dcc5ba4f.jpg"
-    },
-    {
-        id: 13,
-        name: "Crime",
-        img: "https://i.pinimg.com/originals/11/7f/37/117f3707098b5bf7536baa819577ebd6.jpg"
-    },
-    {
-        id: 14,
-        name: "Thriller",
-        img: "https://i.pinimg.com/originals/69/2f/63/692f63406524c70da29703222e541d79.jpg"
-    },
-    {
-        id: 15,
-        name: "Family",
-        img: "https://i.pinimg.com/originals/3f/16/67/3f1667a08da53f422954c0f9f0320f1b.jpg"
-    },
-    {
-        id: 16,
-        name: "War",
-        img: "https://i.pinimg.com/originals/13/4b/e0/134be0f48a05b05bf81f6fe8c7b8fa4e.jpg"
-    },
-    {
-        id: 17,
-        name: "History",
-        img: "https://i.pinimg.com/originals/65/d0/27/65d027640387d4bcb67b546a72634d7b.jpg"
-    }
 ]
